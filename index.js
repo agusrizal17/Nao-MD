3dc0345555997dea0a47f1e627a0222d2b5b6ef9
Agus Rizal <57043034+agusrizal17@users.noreply.github.com>
2024-02-28 21:34:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('Nao-MD ESM', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
-say(`Whatsapp Bot esm by Shirokami Ryzen`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
+say('YZ BOT', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
+say(`Whatsapp Bot by yaelahzal`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
 
 console.log(`
-Nao MD ESM
-Author: Shirokami Ryzen
-WA    : +6285174269046
+YZ BOT
+Author: yaelahzal 
+WA    : +6285869025919
 
-github.com/ShirokamiRyzen/Nao-MD
+github.com/agusrizal17/Nao-MD
 `)
 
 var isRunning = false
